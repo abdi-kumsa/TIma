0c7623ad5ecd626a8bc0bf13d0fa7c11f31fd634
Abdi Kumsa <kumsaaabdii1@gmail.com>
2026-04-24 18:52:12 +0300

    Fix provider ordering to resolve useAuth error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,15 @@ import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
-    <DarkModeProvider>
-      <AuthProvider>
+    <AuthProvider>
+      <DarkModeProvider>
         <TaskProvider>
           <div className="min-h-screen">
             <RouterProvider router={router} />
             <Toaster position="top-center" />
           </div>
         </TaskProvider>
-      </AuthProvider>
-    </DarkModeProvider>
+      </DarkModeProvider>
+    </AuthProvider>
   );
 }
